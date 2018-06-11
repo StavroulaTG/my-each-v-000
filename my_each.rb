62bc170aa465482c7455eb9deec68e133224ad5e
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-11 15:48:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
-def my_each (array)# put argument(s) here
-  array.collect! do |array|
-    puts array
+def my_each(array)# put argument(s) here
+  while my_each 
+    array 
+  else
+    array.collect! do |array|
+      puts array
+    end
   end
 end
 
